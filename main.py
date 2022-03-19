e372565278129fba985c6674abcd36d48b8b7fb5
Yilmaz Mustafa <yilmaz@mail.be>
2022-03-19 23:23:26 +0100

    working on converting code to OOP approach.

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ import mediapipe as mp
 import math
 from cvzone.HandTrackingModule import HandDetector
 
+
+class ImageButton:
+    def __init__(self, imageUrl):
+        self.img = cv2.imread(imageUrl, cv2.IMREAD_UNCHANGED)
+        self.img = cv2.resize(self.img, (0, 0), None, 0.65, 0.65)
+        self.h, self.w, c = self.img.shape
+        self.x = 0
+        self.y = 0
+        self.clickEvent = print
+        self.v = -1
+
+    def text(self, v, img):
+        self.v = v
+        cv2.putText(img, self.v, (self.x + 25, self.y + 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+
+    def defaultClick(self, img):
+        self.text("click!", img)
+
+    def overlay(self, imgBack, posX, posY):
+        self.x = posX
+        self.y = posY
+        return cvzone.overlayPNG(imgBack, self.img, [posX, posY])
+
+    def focused(self, cursorX, cursorY):
+        self.img = cv2.resize(self.img, (0, 0), None, 1.20, 1.20)
+        return self.x < cursorX < self.x + self.w and self.y < cursorY < self.y + self.h
+
+    def onClick(self, clickEvent):
+        self.clickEvent = clickEvent
+
+
 fhd_width = 1920
 fhd_height = 1080
 
@@ -23,8 +54,14 @@ total_no_of_cells = 12
 cell_width = screen_width / total_no_of_cells * 0.65
 cell_height = screen_height / total_no_of_cells * 0.65
 
-imgLogo = cv2.imread("components/logo.png", cv2.IMREAD_UNCHANGED)
-imgLogo = cv2.resize(imgLogo, (0, 0), None, 0.65, 0.65)
+logoBtn = ImageButton("components/logo.png")
+basketBtn = ImageButton("components/basket.png")
+
+# imgLogo = cv2.imread("components/logo.png", cv2.IMREAD_UNCHANGED)
+# imgLogo = cv2.resize(imgLogo, (0, 0), None, 0.65, 0.65)
+
+# imgBasket = cv2.imread("components/basket.png", cv2.IMREAD_UNCHANGED)
+# imgBasket = cv2.resize(imgBasket, (0, 0), None, 0.65, 0.65)
 
 imgHeader = cv2.imread("components/header.png", cv2.IMREAD_UNCHANGED)
 imgHeader = cv2.resize(imgHeader, (0, 0), None, 0.65, 0.65)
@@ -32,29 +69,32 @@ imgHeader = cv2.resize(imgHeader, (0, 0), None, 0.65, 0.65)
 imgFooter = cv2.imread("components/footer.png", cv2.IMREAD_UNCHANGED)
 imgFooter = cv2.resize(imgFooter, (0, 0), None, 0.65, 0.65)
 
-imgPhotoXL = cv2.imread("components/large_product.png", cv2.IMREAD_UNCHANGED)
+imgPhotoXL = cv2.imread("products/large_product.png", cv2.IMREAD_UNCHANGED)
 imgPhotoXL = cv2.resize(imgPhotoXL, (0, 0), None, 0.65, 0.65)
 
-imgProductLeft01 = cv2.imread("components/product_left_01.png", cv2.IMREAD_UNCHANGED)
+imgProductLeft01 = cv2.imread("products/product_left_01.png", cv2.IMREAD_UNCHANGED)
 imgProductLeft01 = cv2.resize(imgProductLeft01, (0, 0), None, 0.65, 0.65)
 
-imgProductLeft02 = cv2.imread("components/product_left_02.png", cv2.IMREAD_UNCHANGED)
+imgProductLeft02 = cv2.imread("products/product_left_02.png", cv2.IMREAD_UNCHANGED)
 imgProductLeft02 = cv2.resize(imgProductLeft02, (0, 0), None, 0.65, 0.65)
 
 imgPreviousButton = cv2.imread("components/previous_button.png", cv2.IMREAD_UNCHANGED)
 imgPreviousButton = cv2.resize(imgPreviousButton, (0, 0), None, 0.65, 0.65)
 
-imgProductRight01 = cv2.imread("components/product_right_01.png", cv2.IMREAD_UNCHANGED)
+imgProductRight01 = cv2.imread("products/product_right_01.png", cv2.IMREAD_UNCHANGED)
 imgProductRight01 = cv2.resize(imgProductRight01, (0, 0), None, 0.65, 0.65)
 
-imgProductRight02 = cv2.imread("components/product_right_02.png", cv2.IMREAD_UNCHANGED)
+imgProductRight02 = cv2.imread("products/product_right_02.png", cv2.IMREAD_UNCHANGED)
 imgProductRight02 = cv2.resize(imgProductRight02, (0, 0), None, 0.65, 0.65)
 
 imgNextButton = cv2.imread("components/next_button.png", cv2.IMREAD_UNCHANGED)
 imgNextButton = cv2.resize(imgNextButton, (0, 0), None, 0.65, 0.65)
 
 hback, wback, cback = imgBack.shape
-hlogo, wlogo, clogo = imgLogo.shape
+
+# hlogo, wlogo, clogo = imgLogo.shape
+# hbasket, wbasket, cbasket = imgBasket.shape
+
 hheader, wheader, cheader = imgHeader.shape
 hfooter, wfooter, cfooter = imgFooter.shape
 hphotoXL, wphotoXL, cphotoXL = imgPhotoXL.shape
@@ -70,15 +110,21 @@ hPrevBtn, wPrevBtn, cPrevBtn = imgPreviousButton.shape
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while cap.isOpened() and success:
+
     success, imgBack = cap.read()
     imgBack = cv2.flip(imgBack, 1)
 
+    # using OOP components
+    imgResult = logoBtn.overlay(imgBack, 0, 0)
+    imgResult = basketBtn.overlay(imgResult, wback - basketBtn.w, 0)
+
     # WebCam or Background Stream
-    imgResult = cvzone.overlayPNG(imgBack, imgLogo, [0, 0])
+    # imgResult = cvzone.overlayPNG(imgBack, imgLogo, [0, 0])
+    # imgResult = cvzone.overlayPNG(imgResult, imgBasket, [wback - wbasket, 0])
 
     # Header and Footer
     imgResult = cvzone.overlayPNG(imgResult, imgHeader, [wback - wheader - 220, 0])
-    imgResult = cvzone.overlayPNG(imgResult, imgFooter, [0, (hback - hfooter)])
+    imgResult = cvzone.overlayPNG(imgResult, imgFooter, [0, hback - hfooter])
 
     # XL Product Info Box
     imgResult = cvzone.overlayPNG(imgResult, imgPhotoXL, [(wback - wphotoXL) // 2, (hback - hphotoXL) // 2])
@@ -86,8 +132,8 @@ while cap.isOpened() and success:
     # Products with Thumbnail Photos
     imgResult = cvzone.overlayPNG(imgResult, imgProductLeft01, [0, 200])
     imgResult = cvzone.overlayPNG(imgResult, imgProductLeft02, [0, 400])
-    imgResult = cvzone.overlayPNG(imgResult, imgProductRight01, [1072, 200])
-    imgResult = cvzone.overlayPNG(imgResult, imgProductRight02, [1072, 400])
+    imgResult = cvzone.overlayPNG(imgResult, imgProductRight01, [wback - wProdR01, 200])
+    imgResult = cvzone.overlayPNG(imgResult, imgProductRight02, [wback - wProdR02, 400])
 
     # detection hands
     hands, imgBack = detector.findHands(imgBack, flipType=False)
